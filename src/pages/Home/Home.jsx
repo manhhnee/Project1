7930a8239201a26eeb69aff24f9e6ec28ab09c49
Manh <manhddt2508@gmail.com>
2024-08-12 19:21:23 +0700

    UI Header

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,5 +1,5 @@
 function Home() {
-  return <h1>This is home page</h1>;
+  return <div></div>;
 }
 
 export default Home;
--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,20 @@ module.exports = {
   darkMode: 'selector',
   content: ['./src/**/*.{js,jsx,ts,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      height: {
+        'desktop-header-height': '74px',
+      },
+      maxWidth: {
+        'desktop-header-max-width': '1230px',
+      },
+      gridTemplateColumns: {
+        'custom-layout': '1fr max-content 1fr',
+      },
+      colors: {
+        'barcelona-header': 'rgba(255, 255, 255, 0.85)',
+      },
+    },
   },
   plugins: [],
 };
